780860a452b9823785f4a94bbd4c4ff6ee0bb1d1
Milos Jelic <milosjelic98@gmail.com>
2022-09-12 15:39:25 +0200

    hotel

---
+++ hotel.sql
@@ -0,0 +1,199 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.14
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 06, 2019 at 08:19 AM
+-- Server version: 5.6.17
+-- PHP Version: 5.5.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `hotel`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contact`
+--
+
+CREATE TABLE IF NOT EXISTS `contact` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `fullname` varchar(100) COLLATE latin1_bin DEFAULT NULL,
+  `phoneno` int(10) DEFAULT NULL,
+  `email` text COLLATE latin1_bin NOT NULL,
+  `cdate` date DEFAULT NULL,
+  `approval` varchar(12) COLLATE latin1_bin DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 COLLATE=latin1_bin AUTO_INCREMENT=3 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login`
+--
+
+CREATE TABLE IF NOT EXISTS `login` (
+  `id` int(10) unsigned NOT NULL,
+  `usname` varchar(30) DEFAULT NULL,
+  `pass` varchar(30) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `login`
+--
+
+INSERT INTO `login` (`id`, `usname`, `pass`) VALUES
+(1, 'Admin', '1234'),
+(2, 'Prasath', '12345'),
+(1, 'Admin', '1234'),
+(2, 'Prasath', '12345'),
+(0, 'milos', '123456'),
+(3, 'milos', '123456');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `newsletterlog`
+--
+
+CREATE TABLE IF NOT EXISTS `newsletterlog` (
+  `id` int(10) unsigned NOT NULL,
+  `title` varchar(52) DEFAULT NULL,
+  `subject` varchar(100) DEFAULT NULL,
+  `news` text
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `newsletterlog`
+--
+
+INSERT INTO `newsletterlog` (`id`, `title`, `subject`, `news`) VALUES
+(0, 'bla', 'bla', 'bla');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payment`
+--
+
+CREATE TABLE IF NOT EXISTS `payment` (
+  `id` int(11) DEFAULT NULL,
+  `title` varchar(5) DEFAULT NULL,
+  `fname` varchar(30) DEFAULT NULL,
+  `lname` varchar(30) DEFAULT NULL,
+  `troom` varchar(30) DEFAULT NULL,
+  `tbed` varchar(30) DEFAULT NULL,
+  `nroom` int(11) DEFAULT NULL,
+  `cin` date DEFAULT NULL,
+  `cout` date DEFAULT NULL,
+  `ttot` double(8,2) DEFAULT NULL,
+  `fintot` double(8,2) DEFAULT NULL,
+  `mepr` double(8,2) DEFAULT NULL,
+  `meal` varchar(30) DEFAULT NULL,
+  `btot` double(8,2) DEFAULT NULL,
+  `noofdays` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `payment`
+--
+
+INSERT INTO `payment` (`id`, `title`, `fname`, `lname`, `troom`, `tbed`, `nroom`, `cin`, `cout`, `ttot`, `fintot`, `mepr`, `meal`, `btot`, `noofdays`) VALUES
+(0, '', 'milos', 'jelic', 'Superior Room', 'Double', 1, '2019-06-07', '2019-06-14', 2240.00, 2284.80, 0.00, 'Room only', 44.80, 7),
+(0, '', 'aleksa', 'ivanovic', 'Superior Room', 'Single', 1, '2019-06-08', '2019-06-10', 640.00, 659.20, 12.80, 'Breakfast', 6.40, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `room`
+--
+
+CREATE TABLE IF NOT EXISTS `room` (
+  `id` int(10) unsigned NOT NULL,
+  `type` varchar(15) DEFAULT NULL,
+  `bedding` varchar(10) DEFAULT NULL,
+  `place` varchar(10) DEFAULT NULL,
+  `cusid` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `room`
+--
+
+INSERT INTO `room` (`id`, `type`, `bedding`, `place`, `cusid`) VALUES
+(1, 'Superior Room', 'Single', 'Free', 0),
+(2, 'Superior Room', 'Double', 'Free', 0),
+(3, 'Superior Room', 'Triple', 'Free', NULL),
+(4, 'Single Room', 'Quad', 'Free', NULL),
+(5, 'Superior Room', 'Quad', 'Free', NULL),
+(6, 'Deluxe Room', 'Single', 'Free', NULL),
+(7, 'Deluxe Room', 'Double', 'Free', NULL),
+(8, 'Deluxe Room', 'Triple', 'Free', NULL),
+(9, 'Deluxe Room', 'Quad', 'Free', NULL),
+(10, 'Guest House', 'Single', 'Free', NULL),
+(11, 'Guest House', 'Double', 'Free', NULL),
+(12, 'Guest House', 'Quad', 'Free', NULL),
+(13, 'Single Room', 'Single', 'Free', NULL),
+(14, 'Single Room', 'Double', 'Free', NULL),
+(15, 'Single Room', 'Triple', 'Free', NULL),
+(1, 'Superior Room', 'Single', 'Free', 0),
+(2, 'Superior Room', 'Double', 'Free', 0),
+(3, 'Superior Room', 'Triple', 'Free', NULL),
+(4, 'Single Room', 'Quad', 'Free', NULL),
+(5, 'Superior Room', 'Quad', 'Free', NULL),
+(6, 'Deluxe Room', 'Single', 'Free', NULL),
+(7, 'Deluxe Room', 'Double', 'Free', NULL),
+(8, 'Deluxe Room', 'Triple', 'Free', NULL),
+(9, 'Deluxe Room', 'Quad', 'Free', NULL),
+(10, 'Guest House', 'Single', 'Free', NULL),
+(11, 'Guest House', 'Double', 'Free', NULL),
+(12, 'Guest House', 'Quad', 'Free', NULL),
+(13, 'Single Room', 'Single', 'Free', NULL),
+(14, 'Single Room', 'Double', 'Free', NULL),
+(15, 'Single Room', 'Triple', 'Free', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `roombook`
+--
+
+CREATE TABLE IF NOT EXISTS `roombook` (
+  `id` int(10) unsigned NOT NULL,
+  `Title` varchar(5) DEFAULT NULL,
+  `FName` text,
+  `LName` text,
+  `Email` varchar(50) DEFAULT NULL,
+  `National` varchar(30) DEFAULT NULL,
+  `Country` varchar(30) DEFAULT NULL,
+  `Phone` text,
+  `TRoom` varchar(20) DEFAULT NULL,
+  `Bed` varchar(10) DEFAULT NULL,
+  `NRoom` varchar(2) DEFAULT NULL,
+  `Meal` varchar(15) DEFAULT NULL,
+  `cin` date DEFAULT NULL,
+  `cout` date DEFAULT NULL,
+  `stat` varchar(15) DEFAULT NULL,
+  `nodays` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `roombook`
+--
+
+INSERT INTO `roombook` (`id`, `Title`, `FName`, `LName`, `Email`, `National`, `Country`, `Phone`, `TRoom`, `Bed`, `NRoom`, `Meal`, `cin`, `cout`, `stat`, `nodays`) VALUES
+(0, NULL, 'milos', 'jelic', 'milosjelic98@gmail.com', 'Serbian', 'Serbia', '0614189611', 'Superior Room', 'Single', '1', 'Breakfast', '2019-06-07', '2019-06-14', 'Not Conform', 7);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
